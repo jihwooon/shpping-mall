8ce238479029121cc563bf7cb6bd7babd844e29d
jihwooon <jihwooon@gmail.com>
2023-08-23 12:03:27 +0900

    product 생성자를 수정하라

--- server/src/product/product.entity.ts
+++ server/src/product/product.entity.ts
@@ -17,27 +17,17 @@ export default class Product {
 
   imageUrl: string
 
-  constructor({
-    productId = 0,
-    categoryId = 0,
-    productName = '',
-    size = '',
-    recentPrice = 0,
-    buyPrice: buyPrice = 0,
-    sellPrice = 0,
-    productInfo = '',
-    imageUrl = '',
-  }: {
-    productId?: number
-    categoryId?: number
-    productName?: string
-    size?: string
-    recentPrice?: number
-    buyPrice?: number
-    sellPrice?: number
-    productInfo?: string
-    imageUrl?: string
-  }) {
+  constructor(
+    productId?: number,
+    categoryId?: number,
+    productName?: string,
+    size?: string,
+    recentPrice?: number,
+    buyPrice?: number,
+    sellPrice?: number,
+    productInfo?: string,
+    imageUrl?: string,
+  ) {
     this.productId = productId
     this.categoryId = categoryId
     this.productName = productName
