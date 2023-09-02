754a6603cf025bbc7e72e1ec554c739eeff21362
jihwooon <jihwooon@gmail.com>
2023-09-02 23:21:15 +0900

    ItemController 테스트 코드를 작성하라

---
+++ server/src/items/item.controller.test.ts
@@ -0,0 +1,40 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { ItemController } from './itemController'
+import { ItemService } from './item.service'
+import { ItemStatusEnum } from './item-status.enum'
+import { ItemRepository } from './item.repository'
+import { DatabaseModule } from '../config/database/database.module'
+
+describe('ItemController', () => {
+  let itemController: ItemController
+  let itemService: ItemService
+  const request = {
+    id: 1,
+    itemName: 'New Balance 530 Steel Grey',
+    itemDetail: 'M990WT6',
+    price: 130000,
+    stockNumber: 10,
+    sellStatus: ItemStatusEnum.SELL,
+  }
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [DatabaseModule],
+      controllers: [ItemController],
+      providers: [ItemService, ItemRepository],
+    }).compile()
+
+    itemController = app.get<ItemController>(ItemController)
+    itemService = app.get<ItemService>(ItemService)
+  })
+
+  describe('createItemHandler', () => {
+    it('should return "Hello World!"', async () => {
+      const spyFn = jest.spyOn(itemController, 'createItemHandler')
+      await itemController.createItemHandler(request)
+
+      expect(spyFn).toHaveBeenCalled()
+      expect(spyFn).toBeCalledWith(request)
+    })
+  })
+})
