a6f075a4b87f5fbc2bb96fb5afd2b0b4da0fde99
jihwooon <jihwooon@gmail.com>
2023-09-04 11:20:27 +0900

    테스트 코드에 mock 데이터를 itemFixture로 옮겨라

--- server/src/items/web/item-create.controller.test.ts
+++ server/src/items/web/item-create.controller.test.ts
@@ -1,23 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { ItemCreateController } from './item-create.controller'
 import { ItemCreater } from '../application/item.creater'
-import { ItemStatusEnum } from '../domain/item-status.enum'
 import { ItemRepository } from '../domain/item.repository'
 import { DatabaseModule } from '../../config/database/database.module'
 import { DbHelper } from '../../config/helper/db.helper'
+import { CREATE_REQUEST } from '../../fixture/itemFixture'
 
 describe('ItemController class', () => {
   let itemController: ItemCreateController
   let itemCreater: ItemCreater
   let dbHelper: DbHelper
-  const REQUEST = {
-    id: 1,
-    itemName: 'New Balance 530 Steel Grey',
-    itemDetail: 'M990WT6',
-    price: 130000,
-    stockNumber: 10,
-    sellStatus: ItemStatusEnum.SELL,
-  }
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -39,10 +31,10 @@ describe('ItemController class', () => {
     context('Item 객체가 주어지면', () => {
       it('해당 메서드 호출을 검증해야 한다', async () => {
         const spyFn = jest.spyOn(itemController, 'createItemHandler').mockImplementation()
-        await itemController.createItemHandler(REQUEST)
+        await itemController.createItemHandler(CREATE_REQUEST)
 
         expect(spyFn).toHaveBeenCalled()
-        expect(spyFn).toBeCalledWith(REQUEST)
+        expect(spyFn).toBeCalledWith(CREATE_REQUEST)
       })
     })
   })
