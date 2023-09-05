4f0e7584774ade52dbc914824f03a5ca7cea4200
jihwooon <jihwooon@gmail.com>
2023-09-06 01:08:49 +0900

    Member 객체를 생성하라
    회원 도메인 객체를 생성했습니다.

---
+++ server/src/fixture/memberFixture.ts
@@ -0,0 +1,17 @@
+import { Role } from '../users/domain/member-role.enum'
+import { MemberType } from '../users/domain/member-type.enum'
+
+export const MEMBER = {
+  memberId: 1,
+  email: 'abc@email.com',
+  memberName: '홍길동',
+  memberType: MemberType.GENERAL,
+  password: '12345678',
+  refreshToken: 'eyJhbGciOiJI',
+  tokenExpirationTime: new Date('2023-09-01T23:10:00.009Z'),
+  role: Role.USER,
+  createTime: new Date('2023-09-01T23:10:00.009Z'),
+  updateTime: new Date('2023-09-01T23:10:00.009Z'),
+  createBy: '홍길동',
+  modifiedBy: '김철수',
+}
