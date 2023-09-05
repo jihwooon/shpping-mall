81df86867b3d8f1ce2ea57924959b33aa328a188
jihwooon <jihwooon@gmail.com>
2023-09-05 18:57:29 +0900

    서버 에러를 해결하라
    기존 서버에서 express로 변경 했는데 서버가 실행되지 않는 문제가 생겼습니다.
    
    기존에 사용하던 서버 구성으로 변경했습니다.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,25 +1,12 @@
-import * as http from 'http'
-import * as express from 'express'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
-import { ExpressAdapter } from '@nestjs/platform-express'
-import * as dotenv from 'dotenv'
-
-dotenv.config()
 
 async function bootstrap() {
-  const server = express()
-  const adapter = new ExpressAdapter(server)
-  const app = await NestFactory.create(AppModule, adapter, {
-    cors: true,
-    logger: ['log', 'error', 'warn', 'debug'],
-  })
+  const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
   const PORT = process.env.PORT ?? 8080
-
-  http.createServer(server).listen(PORT)
-  console.info('server is running on', PORT)
+  await app.listen(PORT)
 }
 bootstrap()
