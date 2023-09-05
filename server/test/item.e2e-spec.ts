6d4c9aba10d3241df970eed44fff216f08626e1c
jihwooon <jihwooon@gmail.com>
2023-09-05 11:42:17 +0900

    PATCH /items/:id의 E2E 테스트를 통과하라
    업데이트 시 E2E 테스트 통과하도록 수정했습니다.

--- server/test/item.e2e-spec.ts
+++ server/test/item.e2e-spec.ts
@@ -3,9 +3,8 @@ import { INestApplication, NotFoundException, ValidationPipe } from '@nestjs/com
 import * as request from 'supertest'
 import { ItemModule } from '../src/items/item.module'
 import { ItemCreater } from '../src/items/application/item.creater'
-import { ItemRepository } from '../src/items/domain/item.repository'
-import { DatabaseModule } from '../src/config/database/database.module'
 import { ItemReader } from '../src/items/application/item.reader'
+import { ItemUpdater } from '../src/items/application/item.updater'
 import {
   CREATE_NOT_DETAIL_REQUEST,
   CREATE_NOT_NAME_REQUEST,
@@ -13,21 +12,27 @@ import {
   CREATE_NOT_STOCK_REQUEST,
   CREATE_REQUEST,
   CREATE_RESPONSE,
+  UPDATE_REQUEST,
+  UPDATE_NOT_NAME_REQUEST,
+  UPDATE_NOT_DETAIL_REQUEST,
+  UPDATE_NOT_PRICE_REQUEST,
+  UPDATE_NOT_STOCK_REQUEST,
 } from '../src/fixture/itemFixture'
 
 describe('ItemController (e2e)', () => {
   let app: INestApplication
   let itemCreater: ItemCreater
   let itemReader: ItemReader
+  let itemUpdater: ItemUpdater
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [ItemModule, DatabaseModule],
-      providers: [ItemCreater, ItemReader, ItemRepository],
+      imports: [ItemModule],
     }).compile()
 
     itemCreater = moduleFixture.get<ItemCreater>(ItemCreater)
     itemReader = moduleFixture.get<ItemReader>(ItemReader)
+    itemUpdater = moduleFixture.get<ItemUpdater>(ItemUpdater)
     app = moduleFixture.createNestApplication()
 
     app.useGlobalPipes(
@@ -145,6 +150,99 @@ describe('ItemController (e2e)', () => {
           error: 'Not Found',
           message: '9999에 해당하는 상품을 찾을 수 없습니다.',
           statusCode: 404,
+        })
+      })
+    })
+  })
+
+  describe('PATCH /items/:id', () => {
+    const ID = 1
+    const NOT_FOUND_ID = 9999
+    context('id와 변경 된 item 요청을 하면', () => {
+      beforeEach(() => {
+        itemUpdater.updateItem = jest.fn().mockImplementation(() => true)
+      })
+
+      it('상태코드 200을 응답한다', async () => {
+        const { status, body } = await request(app.getHttpServer()).patch(`/items/${ID}`).send(UPDATE_REQUEST)
+
+        expect(status).toEqual(200)
+        expect(body).toEqual({})
+      })
+    })
+
+    context('잘못된 id와 변경 된 item 요청을 하면', () => {
+      beforeEach(() => {
+        itemUpdater.updateItem = jest
+          .fn()
+          .mockRejectedValue(new NotFoundException(`${NOT_FOUND_ID}에 해당하는 상품을 찾을 수 없습니다.`))
+      })
+
+      it('상태코드 404을 응답해야 한다', async () => {
+        const { status, body } = await request(app.getHttpServer()).patch(`/items/${NOT_FOUND_ID}`).send(UPDATE_REQUEST)
+
+        expect(status).toEqual(404)
+        expect(body).toEqual({
+          error: 'Not Found',
+          message: '9999에 해당하는 상품을 찾을 수 없습니다.',
+          statusCode: 404,
+        })
+      })
+    })
+
+    context('상풍명을 누락하면', () => {
+      it('상태코드 400를 응답해야 한다', async () => {
+        const { status, body } = await request(app.getHttpServer()).patch(`/items/${ID}`).send(UPDATE_NOT_NAME_REQUEST)
+
+        expect(status).toEqual(400)
+        expect(body).toEqual({
+          error: 'Bad Request',
+          message: ['상품명은 필수 입력 값입니다.'],
+          statusCode: 400,
+        })
+      })
+    })
+
+    context('상품 상세를 누락하면', () => {
+      it('상태코드 400를 응답해야 한다', async () => {
+        const { status, body } = await request(app.getHttpServer())
+          .patch(`/items/${ID}`)
+          .send(UPDATE_NOT_DETAIL_REQUEST)
+
+        expect(status).toEqual(400)
+        expect(body).toEqual({
+          error: 'Bad Request',
+          message: ['상품 상세는 필수 입력 값입니다.'],
+          statusCode: 400,
+        })
+      })
+    })
+
+    context('가격를 누락하면', () => {
+      it('상태코드 400를 응답해야 한다', async () => {
+        const { status, body } = await request(app.getHttpServer()).patch(`/items/${ID}`).send(UPDATE_NOT_PRICE_REQUEST)
+
+        expect(status).toEqual(400)
+        expect(body).toEqual({
+          error: 'Bad Request',
+          message: ['price must be a number conforming to the specified constraints', '가격은 필수 입력 값입니다.'],
+          statusCode: 400,
+        })
+      })
+    })
+
+    context('재고를 누락하면', () => {
+      it('상태코드 400를 응답해야 한다', async () => {
+        const { status, body } = await request(app.getHttpServer()).patch(`/items/${ID}`).send(UPDATE_NOT_STOCK_REQUEST)
+
+        expect(status).toEqual(400)
+        expect(body).toEqual({
+          error: 'Bad Request',
+          message: [
+            'stockNumber must be a number conforming to the specified constraints',
+            '재고는 필수 입력 값입니다.',
+          ],
+          statusCode: 400,
         })
       })
     })
