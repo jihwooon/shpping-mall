79c3eec100c6a4c104adf39d9da8164340484e20
jihwooon <jihwooon@gmail.com>
2023-09-10 16:28:44 +0900

    회원 가입 완료 시 AccessToken을 발급하는 서비스를 작성하라
    회원 가입 정보를 입력하고 이메일 검증이 마치고, 회원 정보가 저장 되면 id 값을 반환합니다.
    회원 정보 id 값을 generateAccessToken에 입력 시 accessToken를 토큰을 발급합니다.

--- server/src/auth/signup/application/signup.service.ts
+++ server/src/auth/signup/application/signup.service.ts
@@ -3,6 +3,8 @@ import { MemberRepository } from '../../../members/domain/member.repository'
 import { EmailChecker } from '../../../members/application/email.checker'
 import { Member } from '../../../members/domain/member.entity'
 import { PasswordProvider } from '../../../members/application/password.provider'
+import { JwtProvider } from '../../../jwt/jwt.provider'
+import { Authentication } from '../dto/authentication'
 
 @Injectable()
 export class SignupService {
@@ -10,14 +12,18 @@ export class SignupService {
     private readonly memberRepository: MemberRepository,
     private readonly emailChecker: EmailChecker,
     private readonly passwordProvider: PasswordProvider,
+    private readonly jwtProvider: JwtProvider,
   ) {}
 
-  async signup(email: string, password: string, memberName: string): Promise<number> {
+  async signup(email: string, password: string, memberName: string): Promise<Authentication> {
     await this.emailChecker.checkDuplicatedEmail(email)
     const hashedPassword = await this.passwordProvider.hashPassword(password)
-
-    return await this.memberRepository.save(
+    const id = await this.memberRepository.save(
       new Member({ email: email, password: hashedPassword, memberName: memberName }),
     )
+
+    return {
+      accessToken: this.jwtProvider.generateAccessToken(id),
+    }
   }
 }
