77404b2943c7007761dbe3c2e0ce0ea723058fce
jihwooon <jihwooon@gmail.com>
2023-09-10 22:54:14 +0900

    토큰을 검증하는 Provider 서비스를 작성하라
    발급 된 accessToken을 검증을 합니다.
    회원 정보가 올바르면 해당 id값을 리턴 합니다.
    그렇지 않으면 에러를 발생합니다.

--- server/src/jwt/jwt.provider.test.ts
+++ server/src/jwt/jwt.provider.test.ts
@@ -1,8 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { JwtProvider } from './jwt.provider'
+import * as jwt from 'jsonwebtoken'
+import { BadRequestException, UnauthorizedException } from '@nestjs/common'
 
 describe('JwtProvider class', () => {
   let jwtProvider: JwtProvider
+
+  let ACCESS_TOKEN =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Im1lbWJlcklkIjoxLCJlbWFpbCI6ImFiY0BlbWFpbC5jb20iLCJtZW1iZXJOYW1lIjoi7ZmN6ri464-ZIiwibWVtYmVyVHlwZSI6IkdFTkVSQUwiLCJwYXNzd29yZCI6IjEyMzQ1Njc4IiwicmVmcmVzaFRva2VuIjoiZXlKaGJHY2lPaUpJIiwidG9rZW5FeHBpcmF0aW9uVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInJvbGUiOiJVU0VSIiwiY3JlYXRlVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInVwZGF0ZVRpbWUiOiIyMDIzLTA5LTAxVDIzOjEwOjAwLjAwOVoiLCJjcmVhdGVCeSI6Iu2Zjeq4uOuPmSIsIm1vZGlmaWVkQnkiOiLquYDssqDsiJgifSwiaWF0IjoxNjk0MzEzOTIyLCJleHAiOjE2OTQ0MDAzMjJ9.F5gN5mkLFk6nET2pJ78_sTdb_YIStT7u8ei7rfK1I7c'
+
+  let INVALID_ACCESS_TOKEN =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Im1lbWJlcklkIjoxLCJlbWFpbCI6ImFiY0BlbWFpbC5jb20iLCJtZW1iZXJOYW1lIjoi7ZmN6ri464-ZIiwibWVtYmVyVHlwZSI6IkdFTkVSQUwiLCJwYXNzd29yZCI6IjEyMzQ1Njc4IiwicmVmcmVzaFRva2VuIjoiZXlKaGJHY2lPaUpJIiwidG9rZW5FeHBpcmF0aW9uVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInJvbGUiOiJVU0VSIiwiY3JlYXRlVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInVwZGF0ZVRpbWUiOiIyMDIzLTA5LTAxVDIzOjEwOjAwLjAwOVoiLCJjcmVhdGVCeSI6Iu2Zjeq4uOuPmSIsIm1vZGlmaWVkQnkiOiLquYDssqDsiJgifSwiaWF0IjoxNjk0MzEzOTIyLCJleHAiOjE2OTQ0MDAzMjJ9.F5gN5mkLFk6nET2pJ78_sTdb_YIStT7u8ei7rfK1I123'
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,6 +29,54 @@ describe('JwtProvider class', () => {
         expect(accessToken).not.toEqual(
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Im1lbWJlcklkIjoxLCJlbWFpbCI6ImFiY0BlbWFpbC5jb20iLCJtZW1iZXJOYW1lIjoi7ZmN6ri464-ZIiwibWVtYmVyVHlwZSI6IkdFTkVSQUwiLCJwYXNzd29yZCI6IjEyMzQ1Njc4IiwicmVmcmVzaFRva2VuIjoiZXlKaGJHY2lPaUpJIiwidG9rZW5FeHBpcmF0aW9uVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInJvbGUiOiJVU0VSIiwiY3JlYXRlVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInVwZGF0ZVRpbWUiOiIyMDIzLTA5LTAxVDIzOjEwOjAwLjAwOVoiLCJjcmVhdGVCeSI6Iu2Zjeq4uOuPmSIsIm1vZGlmaWVkQnkiOiLquYDssqDsiJgifSwiaWF0IjoxNjk0MzEzOTIyLCJleHAiOjE2OTQ0MDAzMjJ9.F5gN5mkLFk6nET2pJ78_sTdb_YIStT7u8ei7rfK1I7c',
         )
+      })
+    })
+
+    context('accessToken이 검증을 성공하면', () => {
+      it('id를 리턴 해야 한다.', () => {
+        const decoded = jwtProvider.validateToken(ACCESS_TOKEN)
+
+        expect(decoded).toEqual(1)
+      })
+    })
+    context('accessToken이 검증을 실패하면', () => {
+      it('JsonWebTokenError을 던져야 한다', () => {
+        try {
+          jwtProvider.validateToken(INVALID_ACCESS_TOKEN)
+        } catch (e) {
+          expect(e).toBeInstanceOf(UnauthorizedException)
+          expect(e.message).toEqual('인증 할 수 없는 token 입니다')
+        }
+      })
+    })
+    context('accessToken이 null 이면', () => {
+      it('BadRequestException을 던져야 한다', () => {
+        try {
+          jwtProvider.validateToken(null)
+        } catch (e) {
+          expect(e).toBeInstanceOf(BadRequestException)
+          expect(e.message).toEqual('token는 null이 될 수 없습니다')
+        }
+      })
+    })
+    context('accessToken이 undefined 이면', () => {
+      it('BadRequestException을 던져야 한다', () => {
+        try {
+          jwtProvider.validateToken(undefined)
+        } catch (e) {
+          expect(e).toBeInstanceOf(BadRequestException)
+          expect(e.message).toEqual('token는 undefined이 될 수 없습니다')
+        }
+      })
+    })
+    context('accessToken이 공백 이면', () => {
+      it('BadRequestException을 던져야 한다', () => {
+        try {
+          jwtProvider.validateToken('')
+        } catch (e) {
+          expect(e).toBeInstanceOf(BadRequestException)
+          expect(e.message).toEqual('token는 이 될 수 없습니다')
+        }
       })
     })
   })
