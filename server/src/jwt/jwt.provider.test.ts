7b0357f603a257ee83e3a3c12ded5d9c1d6ee3d1
jihwooon <jihwooon@gmail.com>
2023-09-14 14:13:28 +0900

    Token을 발급하는 generateToken를 작성하라
    기존 로직은 accessToken과 refreshToken을 따로 분리해서 발급을 했습니다.
    generateToken을 공통화 해서 enum으로 Access와 Refresh를 구분해서 각각 토큰을 따로 분리하게 작성했습니다

--- server/src/jwt/jwt.provider.test.ts
+++ server/src/jwt/jwt.provider.test.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { JwtProvider } from './jwt.provider'
 import { BadRequestException, UnauthorizedException } from '@nestjs/common'
+import { TokenType } from './token-type.enum'
 
 describe('JwtProvider class', () => {
   let jwtProvider: JwtProvider
 
   let INVALID_ACCESS_TOKEN =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Im1lbWJlcklkIjoxLCJlbWFpbCI6ImFiY0BlbWFpbC5jb20iLCJtZW1iZXJOYW1lIjoi7ZmN6ri464-ZIiwibWVtYmVyVHlwZSI6IkdFTkVSQUwiLCJwYXNzd29yZCI6IjEyMzQ1Njc4IiwicmVmcmVzaFRva2VuIjoiZXlKaGJHY2lPaUpJIiwidG9rZW5FeHBpcmF0aW9uVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInJvbGUiOiJVU0VSIiwiY3JlYXRlVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInVwZGF0ZVRpbWUiOiIyMDIzLTA5LTAxVDIzOjEwOjAwLjAwOVoiLCJjcmVhdGVCeSI6Iu2Zjeq4uOuPmSIsIm1vZGlmaWVkQnkiOiLquYDssqDsiJgifSwiaWF0IjoxNjk0MzEzOTIyLCJleHAiOjE2OTQ0MDAzMjJ9.F5gN5mkLFk6nET2pJ78_sTdb_YIStT7u8ei7rfK1I123'
-  const Id = 1
+  const ID = 1
   const ACCESS_TOKEN_EXPIRE = new Date(Date.now() + 86400000)
   const REFRESH_TOKEN_EXPIRE = new Date(Date.now() + 1210500000)
 
@@ -22,7 +23,7 @@ describe('JwtProvider class', () => {
   describe('generateAccessToken method', () => {
     context('사용자 정보가 주어 질 때', () => {
       it('accessToken을 리턴 해야 한다', () => {
-        const accessToken = jwtProvider.generateAccessToken(Id, ACCESS_TOKEN_EXPIRE)
+        const accessToken = jwtProvider.generateAccessToken(ID)
 
         expect(accessToken).not.toEqual(
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Im1lbWJlcklkIjoxLCJlbWFpbCI6ImFiY0BlbWFpbC5jb20iLCJtZW1iZXJOYW1lIjoi7ZmN6ri464-ZIiwibWVtYmVyVHlwZSI6IkdFTkVSQUwiLCJwYXNzd29yZCI6IjEyMzQ1Njc4IiwicmVmcmVzaFRva2VuIjoiZXlKaGJHY2lPaUpJIiwidG9rZW5FeHBpcmF0aW9uVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInJvbGUiOiJVU0VSIiwiY3JlYXRlVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInVwZGF0ZVRpbWUiOiIyMDIzLTA5LTAxVDIzOjEwOjAwLjAwOVoiLCJjcmVhdGVCeSI6Iu2Zjeq4uOuPmSIsIm1vZGlmaWVkQnkiOiLquYDssqDsiJgifSwiaWF0IjoxNjk0MzEzOTIyLCJleHAiOjE2OTQ0MDAzMjJ9.F5gN5mkLFk6nET2pJ78_sTdb_YIStT7u8ei7rfK1I7c',
@@ -32,7 +33,7 @@ describe('JwtProvider class', () => {
     context('id가 null이 주어지면', () => {
       it('BadRequestException를 던져야 한다', () => {
         try {
-          jwtProvider.generateAccessToken(null, ACCESS_TOKEN_EXPIRE)
+          jwtProvider.generateAccessToken(null)
         } catch (e) {
           expect(e).toBeInstanceOf(BadRequestException)
           expect(e.message).toEqual('id는 null이 될 수 없습니다')
@@ -42,7 +43,7 @@ describe('JwtProvider class', () => {
     context('id가 undefined이 주어지면', () => {
       it('BadRequestException를 던져야 한다', () => {
         try {
-          jwtProvider.generateAccessToken(undefined, ACCESS_TOKEN_EXPIRE)
+          jwtProvider.generateAccessToken(undefined)
         } catch (e) {
           expect(e).toBeInstanceOf(BadRequestException)
           expect(e.message).toEqual('id는 undefined이 될 수 없습니다')
@@ -54,7 +55,7 @@ describe('JwtProvider class', () => {
   describe('generateRefreshToken method', () => {
     context('사용자 정보가 주어 질 때', () => {
       it('refreshToken을 리턴 해야 한다', () => {
-        const refreshToken = jwtProvider.generateRefreshToken(Id, REFRESH_TOKEN_EXPIRE)
+        const refreshToken = jwtProvider.generateRefreshToken(ID)
 
         expect(refreshToken).not.toEqual(
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7Im1lbWJlcklkIjoxLCJlbWFpbCI6ImFiY0BlbWFpbC5jb20iLCJtZW1iZXJOYW1lIjoi7ZmN6ri464-ZIiwibWVtYmVyVHlwZSI6IkdFTkVSQUwiLCJwYXNzd29yZCI6IjEyMzQ1Njc4IiwicmVmcmVzaFRva2VuIjoiZXlKaGJHY2lPaUpJIiwidG9rZW5FeHBpcmF0aW9uVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInJvbGUiOiJVU0VSIiwiY3JlYXRlVGltZSI6IjIwMjMtMDktMDFUMjM6MTA6MDAuMDA5WiIsInVwZGF0ZVRpbWUiOiIyMDIzLTA5LTAxVDIzOjEwOjAwLjAwOVoiLCJjcmVhdGVCeSI6Iu2Zjeq4uOuPmSIsIm1vZGlmaWVkQnkiOiLquYDssqDsiJgifSwiaWF0IjoxNjk0MzEzOTIyLCJleHAiOjE2OTQ0MDAzMjJ9.F5gN5mkLFk6nET2pJ78_sTdb_YIStT7u8ei7rfK1I7c',
@@ -64,7 +65,7 @@ describe('JwtProvider class', () => {
     context('id가 null이 주어지면', () => {
       it('BadRequestException를 던져야 한다', () => {
         try {
-          jwtProvider.generateRefreshToken(null, REFRESH_TOKEN_EXPIRE)
+          jwtProvider.generateRefreshToken(null)
         } catch (e) {
           expect(e).toBeInstanceOf(BadRequestException)
           expect(e.message).toEqual('id는 null이 될 수 없습니다')
@@ -74,7 +75,7 @@ describe('JwtProvider class', () => {
     context('id가 undefined이 주어지면', () => {
       it('BadRequestException를 던져야 한다', () => {
         try {
-          jwtProvider.generateRefreshToken(undefined, REFRESH_TOKEN_EXPIRE)
+          jwtProvider.generateRefreshToken(undefined)
         } catch (e) {
           expect(e).toBeInstanceOf(BadRequestException)
           expect(e.message).toEqual('id는 undefined이 될 수 없습니다')
@@ -86,7 +87,8 @@ describe('JwtProvider class', () => {
   describe('validateToken method', () => {
     context('accessToken이 검증을 성공하면', () => {
       it('id를 리턴 해야 한다.', () => {
-        const payload = jwtProvider.validateToken(jwtProvider.generateAccessToken(Id, ACCESS_TOKEN_EXPIRE))
+        const generateAccessToken = jwtProvider.generateAccessToken(ID)
+        const payload = jwtProvider.validateToken(generateAccessToken.accessToken)
 
         expect(payload).toEqual(1)
       })
@@ -149,6 +151,27 @@ describe('JwtProvider class', () => {
         const refreshTokenExpireTime = jwtProvider.createRefreshTokenExpireTime()
 
         expect(refreshTokenExpireTime).toBeTruthy()
+      })
+    })
+  })
+
+  describe('generateToken method', () => {
+    context('id와 accessToken 만료 시간, Access Type이 주어지면', () => {
+      it('AccessToken을 리턴해야 한다', () => {
+        const accessToken = jwtProvider.generateToken(ID, ACCESS_TOKEN_EXPIRE, TokenType.ACCESS)
+
+        expect(accessToken).not.toEqual(
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjoxLCJpYXQiOjE2OTQ2NjY2MTksImV4cCI6MTY5NDY2NzI5Mywic3ViIjoiQUNDRVNTIn0.yrr0muDm32ddItUb3HAAR_bDapXp3t-tKFApU2Wowh8',
+        )
+      })
+    })
+    context('id와 refreshToken 만료 시간, Refresh Type이 주어지면', () => {
+      it('RefreshToken을 리턴해야 한다', () => {
+        const refreshToken = jwtProvider.generateToken(ID, REFRESH_TOKEN_EXPIRE, TokenType.REFRESH)
+
+        expect(refreshToken).not.toEqual(
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjoxLCJpYXQiOjE2OTQ2NjY2NDUsImV4cCI6MTY5NDY2NzQwNywic3ViIjoiUkVGUkVTSCJ9.twLp5ynQZaiAUF9DHNk9Z2xh-GaDeF5QAxk4JA2qW1I',
+        )
       })
     })
   })
