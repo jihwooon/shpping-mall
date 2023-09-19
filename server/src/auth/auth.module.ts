f346e3fe2855773511dd19a54a4a67cc3b9a03d3
jihwooon <jihwooon@gmail.com>
2023-09-20 02:35:10 +0900

    auth module에 SignoutController와 SignoutService를 import를 하라

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -8,10 +8,12 @@ import { SigninController } from './signin/web/signin.controller'
 import { SigninService } from './signin/application/signin.service'
 import { TokenController } from './token/web/token.controller'
 import { TokenIssuer } from './token/application/token.issuer'
+import { SignoutService } from './signout/application/signout.service'
+import { SignoutController } from './signout/web/signout.controller'
 
 @Module({
   imports: [DatabaseModule, MemberModule],
-  controllers: [SignupController, SigninController, TokenController],
-  providers: [SignupService, SigninService, JwtProvider, TokenIssuer],
+  controllers: [SignupController, SigninController, TokenController, SignoutController],
+  providers: [SignupService, SigninService, JwtProvider, TokenIssuer, SignoutService],
 })
 export class AuthModule {}
