c4d53bac87198006a5c711529811c2e88d07083c
jihwooon <jihwooon@gmail.com>
2023-09-24 23:37:33 +0900

    item e2e 테스터 mock 데이터를 수정하라

--- server/test/item.e2e-spec.ts
+++ server/test/item.e2e-spec.ts
@@ -4,25 +4,16 @@ import * as request from 'supertest'
 import { ItemCreater } from '../src/items/application/item.creater'
 import { ItemReader } from '../src/items/application/item.reader'
 import { ItemUpdater } from '../src/items/application/item.updater'
-import {
-  CREATE_NOT_DETAIL_REQUEST,
-  CREATE_NOT_NAME_REQUEST,
-  CREATE_NOT_PRICE_REQUEST,
-  CREATE_NOT_STOCK_REQUEST,
-  CREATE_REQUEST,
-  CREATE_RESPONSE,
-  UPDATE_NOT_DETAIL_REQUEST,
-  UPDATE_NOT_NAME_REQUEST,
-  UPDATE_NOT_PRICE_REQUEST,
-  UPDATE_NOT_STOCK_REQUEST,
-  UPDATE_REQUEST,
-} from '../src/fixture/itemFixture'
+import { itemMock } from '../src/fixture/itemFixture'
 import { ItemUpdateController } from '../src/items/web/item-update.controller'
 import { ItemDetailController } from '../src/items/web/item-detail.controller'
 import { ItemCreateController } from '../src/items/web/item-create.controller'
 import { ItemRepository } from '../src/items/domain/item.repository'
 import { MYSQL_CONNECTION } from '../src/config/database/constants'
 import { Connection } from 'mysql2/promise'
+import { CreateItemRequest } from '../src/items/dto/save-item.dto'
+import { ItemResponse } from '../src/items/dto/detail-item.dto'
+import { UpdateItemRequest } from '../src/items/dto/update-item.dto'
 
 describe('ItemController (e2e)', () => {
   let app: INestApplication
@@ -69,7 +60,15 @@ describe('ItemController (e2e)', () => {
 
     context('Item 객체가 주어지면', () => {
       it('상태코드 204를 응답해야 한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).post('/items').send(CREATE_REQUEST)
+        const itemRequest: CreateItemRequest = {
+          itemName: itemMock().itemName,
+          itemDetail: itemMock().itemDetail,
+          price: itemMock().price,
+          stockNumber: itemMock().stockNumber,
+          sellStatus: itemMock().itemSellStatus,
+        }
+
+        const { status, body } = await request(app.getHttpServer()).post('/items').send(itemRequest)
 
         expect(status).toEqual(204)
         expect(body).toEqual({})
@@ -78,7 +77,15 @@ describe('ItemController (e2e)', () => {
 
     context('상풍명을 누락하면', () => {
       it('상태코드 400를 응답해야 한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).post('/items').send(CREATE_NOT_NAME_REQUEST)
+        const blank_itemName = ''
+        const itemRequest: CreateItemRequest = {
+          itemName: blank_itemName,
+          itemDetail: itemMock().itemDetail,
+          price: itemMock().price,
+          stockNumber: itemMock().stockNumber,
+          sellStatus: itemMock().itemSellStatus,
+        }
+        const { status, body } = await request(app.getHttpServer()).post('/items').send(itemRequest)
 
         expect(status).toEqual(400)
         expect(body).toEqual({
@@ -91,7 +98,15 @@ describe('ItemController (e2e)', () => {
 
     context('상품 상세를 누락하면', () => {
       it('상태코드 400를 응답해야 한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).post('/items').send(CREATE_NOT_DETAIL_REQUEST)
+        const blank_itemDetail = ''
+        const itemRequest: CreateItemRequest = {
+          itemName: itemMock().itemName,
+          itemDetail: blank_itemDetail,
+          price: itemMock().price,
+          stockNumber: itemMock().stockNumber,
+          sellStatus: itemMock().itemSellStatus,
+        }
+        const { status, body } = await request(app.getHttpServer()).post('/items').send(itemRequest)
 
         expect(status).toEqual(400)
         expect(body).toEqual({
@@ -101,9 +116,18 @@ describe('ItemController (e2e)', () => {
         })
       })
     })
+
     context('가격를 누락하면', () => {
       it('상태코드 400를 응답해야 한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).post('/items').send(CREATE_NOT_PRICE_REQUEST)
+        const blank_price = undefined
+        const itemRequest: CreateItemRequest = {
+          itemName: itemMock().itemName,
+          itemDetail: itemMock().itemDetail,
+          price: blank_price,
+          stockNumber: itemMock().stockNumber,
+          sellStatus: itemMock().itemSellStatus,
+        }
+        const { status, body } = await request(app.getHttpServer()).post('/items').send(itemRequest)
 
         expect(status).toEqual(400)
         expect(body).toEqual({
@@ -113,9 +137,18 @@ describe('ItemController (e2e)', () => {
         })
       })
     })
+
     context('재고를 누락하면', () => {
       it('상태코드 400를 응답해야 한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).post('/items').send(CREATE_NOT_STOCK_REQUEST)
+        const blank_stockNumber = undefined
+        const itemRequest: CreateItemRequest = {
+          itemName: itemMock().itemName,
+          itemDetail: itemMock().itemDetail,
+          price: itemMock().price,
+          stockNumber: blank_stockNumber,
+          sellStatus: itemMock().itemSellStatus,
+        }
+        const { status, body } = await request(app.getHttpServer()).post('/items').send(itemRequest)
 
         expect(status).toEqual(400)
         expect(body).toEqual({
@@ -131,36 +164,36 @@ describe('ItemController (e2e)', () => {
   })
 
   describe('GET /items/:id', () => {
-    const ID = 1
-    const NOT_FOUND_ID = 9999
+    const itemResponse: ItemResponse = {
+      id: itemMock().id,
+      itemName: itemMock().itemName,
+      itemDetail: itemMock().itemDetail,
+      price: itemMock().price,
+      stockNumber: itemMock().stockNumber,
+      sellStatus: itemMock().itemSellStatus,
+    }
     context('id 요청을 하면', () => {
       beforeEach(() => {
-        itemReader.getItem = jest.fn().mockImplementation(() => CREATE_RESPONSE)
+        itemReader.getItem = jest.fn().mockImplementation(() => itemResponse)
       })
 
       it('상태코드 200을 응답해야 한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).get(`/items/${ID}`)
+        const { status, body } = await request(app.getHttpServer()).get(`/items/${itemMock().id}`)
 
         expect(status).toEqual(200)
-        expect(body).toEqual({
-          id: 1,
-          itemDetail: 'M990WT6',
-          itemName: 'New Balance 530 Steel Grey',
-          price: 130000,
-          sellStatus: 'SELL',
-          stockNumber: 10,
-        })
+        expect(body).toEqual(itemResponse)
       })
     })
 
     context('잘못된 id 요청을 하면', () => {
+      const not_found_id = (itemMock().id = 9999)
       beforeEach(() => {
         itemReader.getItem = jest
           .fn()
-          .mockRejectedValue(new NotFoundException(`${NOT_FOUND_ID}에 해당하는 상품을 찾을 수 없습니다.`))
+          .mockRejectedValue(new NotFoundException(`${not_found_id}에 해당하는 상품을 찾을 수 없습니다.`))
       })
       it('상태코드 404를 응답해야 한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).get(`/items/${NOT_FOUND_ID}`)
+        const { status, body } = await request(app.getHttpServer()).get(`/items/${not_found_id}`)
 
         expect(status).toEqual(404)
         expect(body).toEqual({
@@ -173,15 +206,22 @@ describe('ItemController (e2e)', () => {
   })
 
   describe('PATCH /items/:id', () => {
-    const ID = 1
-    const NOT_FOUND_ID = 9999
     context('id와 변경 된 item 요청을 하면', () => {
       beforeEach(() => {
         itemUpdater.updateItem = jest.fn().mockImplementation(() => true)
       })
 
       it('상태코드 200을 응답한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).patch(`/items/${ID}`).send(UPDATE_REQUEST)
+        const updateItemRequest: UpdateItemRequest = {
+          itemName: itemMock().itemName,
+          itemDetail: itemMock().itemDetail,
+          sellStatus: itemMock().itemSellStatus,
+          price: itemMock().price,
+          stockNumber: itemMock().stockNumber,
+        }
+        const { status, body } = await request(app.getHttpServer())
+          .patch(`/items/${itemMock().id}`)
+          .send(updateItemRequest)
 
         expect(status).toEqual(200)
         expect(body).toEqual({})
@@ -189,14 +229,24 @@ describe('ItemController (e2e)', () => {
     })
 
     context('잘못된 id와 변경 된 item 요청을 하면', () => {
+      const not_found_id = (itemMock().id = 9999)
       beforeEach(() => {
         itemUpdater.updateItem = jest
           .fn()
-          .mockRejectedValue(new NotFoundException(`${NOT_FOUND_ID}에 해당하는 상품을 찾을 수 없습니다.`))
+          .mockRejectedValue(new NotFoundException(`${not_found_id}에 해당하는 상품을 찾을 수 없습니다.`))
       })
 
       it('상태코드 404을 응답해야 한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).patch(`/items/${NOT_FOUND_ID}`).send(UPDATE_REQUEST)
+        const updateItemRequest: UpdateItemRequest = {
+          itemName: itemMock().itemName,
+          itemDetail: itemMock().itemDetail,
+          sellStatus: itemMock().itemSellStatus,
+          price: itemMock().price,
+          stockNumber: itemMock().stockNumber,
+        }
+        const { status, body } = await request(app.getHttpServer())
+          .patch(`/items/${not_found_id}`)
+          .send(updateItemRequest)
 
         expect(status).toEqual(404)
         expect(body).toEqual({
@@ -209,7 +259,17 @@ describe('ItemController (e2e)', () => {
 
     context('상풍명을 누락하면', () => {
       it('상태코드 400를 응답해야 한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).patch(`/items/${ID}`).send(UPDATE_NOT_NAME_REQUEST)
+        const not_found_itemName = (itemMock().itemName = '')
+        const updateItemRequest: UpdateItemRequest = {
+          itemName: not_found_itemName,
+          itemDetail: itemMock().itemDetail,
+          sellStatus: itemMock().itemSellStatus,
+          price: itemMock().price,
+          stockNumber: itemMock().stockNumber,
+        }
+        const { status, body } = await request(app.getHttpServer())
+          .patch(`/items/${itemMock().id}`)
+          .send(updateItemRequest)
 
         expect(status).toEqual(400)
         expect(body).toEqual({
@@ -222,9 +282,17 @@ describe('ItemController (e2e)', () => {
 
     context('상품 상세를 누락하면', () => {
       it('상태코드 400를 응답해야 한다', async () => {
+        const not_found_itemDetail = (itemMock().itemDetail = '')
+        const updateItemRequest: UpdateItemRequest = {
+          itemName: itemMock().itemName,
+          itemDetail: not_found_itemDetail,
+          sellStatus: itemMock().itemSellStatus,
+          price: itemMock().price,
+          stockNumber: itemMock().stockNumber,
+        }
         const { status, body } = await request(app.getHttpServer())
-          .patch(`/items/${ID}`)
-          .send(UPDATE_NOT_DETAIL_REQUEST)
+          .patch(`/items/${itemMock().id}`)
+          .send(updateItemRequest)
 
         expect(status).toEqual(400)
         expect(body).toEqual({
@@ -237,7 +305,18 @@ describe('ItemController (e2e)', () => {
 
     context('가격를 누락하면', () => {
       it('상태코드 400를 응답해야 한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).patch(`/items/${ID}`).send(UPDATE_NOT_PRICE_REQUEST)
+        const not_found_price = (itemMock().price = undefined)
+        const updateItemRequest: UpdateItemRequest = {
+          itemName: itemMock().itemName,
+          itemDetail: itemMock().itemDetail,
+          sellStatus: itemMock().itemSellStatus,
+          price: not_found_price,
+          stockNumber: itemMock().stockNumber,
+        }
+
+        const { status, body } = await request(app.getHttpServer())
+          .patch(`/items/${itemMock().id}`)
+          .send(updateItemRequest)
 
         expect(status).toEqual(400)
         expect(body).toEqual({
@@ -250,7 +329,17 @@ describe('ItemController (e2e)', () => {
 
     context('재고를 누락하면', () => {
       it('상태코드 400를 응답해야 한다', async () => {
-        const { status, body } = await request(app.getHttpServer()).patch(`/items/${ID}`).send(UPDATE_NOT_STOCK_REQUEST)
+        const not_found_stockName = (itemMock().stockNumber = undefined)
+        const updateItemRequest: UpdateItemRequest = {
+          itemName: itemMock().itemName,
+          itemDetail: itemMock().itemDetail,
+          sellStatus: itemMock().itemSellStatus,
+          price: itemMock().price,
+          stockNumber: not_found_stockName,
+        }
+        const { status, body } = await request(app.getHttpServer())
+          .patch(`/items/${itemMock().id}`)
+          .send(updateItemRequest)
 
         expect(status).toEqual(400)
         expect(body).toEqual({
