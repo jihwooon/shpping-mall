f80f82b7fdc051c5f199bf8967908cdc0a4fb407
jihwooon <jihwooon@gmail.com>
2023-09-27 20:53:54 +0900

    jest-when 라이브러리를 사용해서 item create 테스트 코드를 수정하라
    jest-when 라이브러리를 사용해서 테스트 가독성을 높였습니다

--- server/src/items/application/item.creater.test.ts
+++ server/src/items/application/item.creater.test.ts
@@ -8,37 +8,49 @@ import { InternalServerErrorException } from '@nestjs/common'
 import { userMock } from '../../fixture/memberFixture'
 import { MemberRepository } from '../../members/domain/member.repository'
 import { MemberNotFoundException } from '../../members/application/error/member-not-found.exception'
+import { when } from 'jest-when'
 
 describe('ItemCreater class', () => {
   let itemCreater: ItemCreater
-  let itemRepository: ItemRepository
-  let memberRepository: MemberRepository
   let connection: Connection
+
+  const ItemRepositoryMock = {
+    save: jest.fn(),
+  }
+
+  const MemberRepositoryMock = {
+    findByEmail: jest.fn(),
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        ItemRepository,
         ItemCreater,
-        MemberRepository,
         {
           provide: MYSQL_CONNECTION,
           useValue: connection,
+        },
+        {
+          provide: ItemRepository,
+          useValue: ItemRepositoryMock,
+        },
+        {
+          provide: MemberRepository,
+          useValue: MemberRepositoryMock,
         },
       ],
     }).compile()
 
     itemCreater = module.get<ItemCreater>(ItemCreater)
-    itemRepository = module.get<ItemRepository>(ItemRepository)
-    memberRepository = module.get<MemberRepository>(MemberRepository)
   })
 
   describe('registerItem method', () => {
-    beforeEach(async () => {
-      itemRepository.save = jest.fn().mockResolvedValue(itemMock().id)
-      memberRepository.findByEmail = jest.fn().mockResolvedValue(itemMock())
+    beforeEach(() => {
+      when(ItemRepositoryMock.save).calledWith(expect.anything()).mockResolvedValue(itemMock().id)
+      when(MemberRepositoryMock.findByEmail).calledWith(userMock().email).mockResolvedValue(itemMock())
     })
-    context('Item 객체가 주어지고 저장을 성공하면', () => {
+
+    context('상품 정보가 주어지고 저장을 성공하면', () => {
       it('저장된 id 값을 리턴해야 한다', async () => {
         const id = await itemCreater.registerItem(itemMock(), userMock().email)
 
@@ -46,30 +58,27 @@ describe('ItemCreater class', () => {
       })
     })
 
-    context('Item 객체가 주어지고 저장을 실패하면', () => {
+    context('상품 정보가 주어지고 저장을 실패하면', () => {
       beforeEach(async () => {
-        itemRepository.save = jest
-          .fn()
-          .mockRejectedValue(new InternalServerErrorException('예기치 못한 서버 오류가 발생했습니다'))
-        memberRepository.findByEmail = jest.fn().mockResolvedValue(true)
+        when(ItemRepositoryMock.save).mockImplementation(() => {
+          throw new InternalServerErrorException('예기치 못한 서버 오류가 발생했습니다')
+        })
       })
-      it('InternalServerErrorException르 던져야 한다', async () => {
+      it('InternalServerErrorException를 던져야 한다', async () => {
         expect(itemCreater.registerItem(itemMock(), userMock().email)).rejects.toThrow(
           new InternalServerErrorException('예기치 못한 서버 오류가 발생했습니다'),
         )
       })
     })
-  })
 
-  context('Item 객체가 주어지고 저장을 실패하면', () => {
-    beforeEach(async () => {
-      itemRepository.save = jest.fn().mockRejectedValue(new MemberNotFoundException('회원 정보를 찾을 수 없습니다'))
-      memberRepository.findByEmail = jest.fn().mockResolvedValue(undefined)
-    })
-    it('InternalServerErrorException르 던져야 한다', async () => {
-      expect(itemCreater.registerItem(itemMock(), userMock().email)).rejects.toThrow(
-        new MemberNotFoundException('회원 정보를 찾을 수 없습니다'),
-      )
+    context('상품 정보가 주어지고 이메일이 올바르지 않으면', () => {
+      it('MemberNotFoundException를 던져야 한다', async () => {
+        const not_found_email = (userMock().email = 'defg@email.com')
+
+        expect(itemCreater.registerItem(itemMock(), not_found_email)).rejects.toThrow(
+          new MemberNotFoundException('회원 정보를 찾을 수 없습니다'),
+        )
+      })
     })
   })
 })
--- server/src/items/web/item-create.controller.test.ts
+++ server/src/items/web/item-create.controller.test.ts
@@ -7,35 +7,42 @@ import { MYSQL_CONNECTION } from '../../config/database/constants'
 import { Connection } from 'mysql2/promise'
 import { CreateItemRequest } from '../dto/save-item.dto'
 import { InternalServerErrorException } from '@nestjs/common'
-import { MemberRepository } from '../../members/domain/member.repository'
 import { JwtProvider } from '../../jwt/jwt.provider'
+import { when } from 'jest-when'
+import { userMock } from '../../fixture/memberFixture'
 
 describe('ItemController class', () => {
   let itemController: ItemCreateController
-  let itemCreater: ItemCreater
   let connection: Connection
 
   const requestMock = {
-    user: jest.fn((x) => x),
+    user: {
+      email: userMock().email,
+    },
   } as unknown as Request
+
+  const ItemCreaterMock = {
+    registerItem: jest.fn(),
+  }
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [ItemCreateController],
       providers: [
-        ItemCreater,
         ItemRepository,
-        MemberRepository,
         JwtProvider,
         {
           provide: MYSQL_CONNECTION,
           useValue: connection,
         },
+        {
+          provide: ItemCreater,
+          useValue: ItemCreaterMock,
+        },
       ],
     }).compile()
 
     itemController = app.get<ItemCreateController>(ItemCreateController)
-    itemCreater = app.get<ItemCreater>(ItemCreater)
   })
 
   describe('createItemHandler method', () => {
@@ -46,10 +53,10 @@ describe('ItemController class', () => {
       stockNumber: itemMock().stockNumber,
       sellStatus: itemMock().itemSellStatus,
     }
-    context('Item 객체가 주어지고 저장을 성공하면', () => {
-      beforeEach(() => {
-        itemCreater.registerItem = jest.fn().mockResolvedValue(itemMock().id)
-      })
+    beforeEach(() => {
+      when(ItemCreaterMock.registerItem).mockResolvedValue(itemMock().id)
+    })
+    context('상품 정보가 주어지고 저장을 성공하면', () => {
       it('id를 리턴해야 한다', async () => {
         const id = await itemController.createItemHandler(itemRequest, requestMock)
 
@@ -59,11 +66,11 @@ describe('ItemController class', () => {
       })
     })
 
-    context('Item 객체가 주어지고 저장을 실패하면', () => {
+    context('상품 정보가 주어지고 저장을 실패하면', () => {
       beforeEach(() => {
-        itemCreater.registerItem = jest
-          .fn()
-          .mockRejectedValue(new InternalServerErrorException('예기치 못한 서버 오류가 발생했습니다'))
+        when(ItemCreaterMock.registerItem).mockImplementation(() => {
+          throw new InternalServerErrorException('예기치 못한 서버 오류가 발생했습니다')
+        })
       })
       it('InternalServerErrorException를 던져야 한다', async () => {
         expect(itemController.createItemHandler(itemRequest, requestMock)).rejects.toThrow(
