7b3c44e70ddbbc1e8ca843067fac726efd6b2ca9
jihwooon <jihwooon@gmail.com>
2023-10-23 23:21:21 +0900

    item 모듈에 MulterModule를 설정하라
    MulterModule Option을 설정을 했습니다.
    파일 저장 위치, 저장 한 파일명, 크기 제한, 파일 갯수 제한을 옵션으로 설정을 했습니다.

--- server/src/items/item.module.ts
+++ server/src/items/item.module.ts
@@ -11,9 +11,29 @@ import { MemberRepository } from '../members/domain/member.repository'
 import { JwtProvider } from '../jwt/jwt.provider'
 import { ItemImageService } from '../item-images/application/item-image.service'
 import { ItemImageRepository } from '../item-images/domain/item-image.repository'
+import { MulterModule } from '@nestjs/platform-express'
+import { diskStorage } from 'multer'
+import * as mime from 'mime-types'
+import { v4 as uuid } from 'uuid'
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    MulterModule.register({
+      storage: diskStorage({
+        destination(req, file, callback) {
+          callback(null, './uploads')
+        },
+        filename(req, file, callback) {
+          callback(null, `${uuid()}.${mime.extension(file.mimetype)}`)
+        },
+      }),
+      limits: {
+        fileSize: 1024 * 1024 * 5,
+        files: 5,
+      },
+    }),
+  ],
   controllers: [ItemCreateController, ItemDetailController, ItemUpdateController],
   providers: [
     ItemCreater,
