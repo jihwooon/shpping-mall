8eebacd074c8e4cd1b7d7b94c0e428c72fc79f2c
jihwooon <jihwooon@gmail.com>
2023-10-31 21:31:34 +0900

    이미지 파일 변경 시 기존 파일을 변경하는 기능을 ItemUpdateController에 추가하라
    이미지 파일을 변경 시 기존 저장 된 이미지 파일을 대체하는 하도록 로직을 추가했습니다

--- server/src/items/web/item-update.controller.ts
+++ server/src/items/web/item-update.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Patch, Param, Body, UseGuards, Req } from '@nestjs/common'
+import { Body, Controller, Param, Patch, Req, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common'
 import { ItemUpdater } from '../application/item.updater'
 import { UpdateItemRequest } from '../dto/update-item.dto'
 import { Item } from '../domain/item.entity'
+import { FilesInterceptor } from '@nestjs/platform-express'
 import { JwtAuthGuard } from '../../config/auth/guards/jwt-auth.guard'
 import { RolesGuard } from '../../config/auth/guards/role-auth.guard'
 import { Roles } from '../../config/auth/role.decorator'
 import { Role } from '../../members/domain/member-role.enum'
+import { multerDiskOptions } from '../../config/multer/multer.options'
 
 @Controller('items')
 export class ItemUpdateController {
@@ -14,12 +16,14 @@ export class ItemUpdateController {
   @Patch(':id')
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
+  @UseInterceptors(FilesInterceptor('files', null, multerDiskOptions))
   async updateItemHandler(
     @Param('id') id: string,
+    @UploadedFiles() files: Express.Multer.File[],
     @Req() req: any,
-    @Body() request: UpdateItemRequest,
+    @Body() dto: UpdateItemRequest,
   ): Promise<boolean> {
-    const { itemName, itemDetail, price, stockNumber, sellStatus } = request
+    const { itemName, itemDetail, price, stockNumber, sellStatus } = dto
     const { email } = req?.user
 
     return await this.itemService.updateItem(
@@ -32,6 +36,7 @@ export class ItemUpdateController {
         stockNumber: stockNumber,
         sellStatus: sellStatus,
       }),
+      files,
     )
   }
 }
