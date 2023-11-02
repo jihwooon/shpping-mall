04fe31adfe10d5dfccd0baf8c37992326bb25177
jihwooon <jihwooon@gmail.com>
2023-11-02 14:25:37 +0900

    Exception 폴더 경로를 수정하라
    Exception 경로를 수정했습니다

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@nestjs/core'
 import { ConfigModule } from '@nestjs/config'
 import { ItemModule } from './items/item.module'
 import { AuthModule } from './auth/auth.module'
-import { ExceptionModule } from './config/error/exception.module'
+import { ExceptionModule } from './exception/exception.module'
 
 @Module({
   imports: [
