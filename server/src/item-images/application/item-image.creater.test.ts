bbf4e9bcf3e1b766fce877ac0b9ed22fdd843d13
jihwooon <jihwooon@gmail.com>
2023-11-02 18:49:23 +0900

    ItemImageCreater 테스트코드를 작성하라
    메서드 반환 타입이 void 형태로 반환값이 없습니다.
    메서드 호출여부를 테스트를 진행했습니다

--- server/src/item-images/application/item-image.creater.test.ts
+++ server/src/item-images/application/item-image.creater.test.ts
@@ -2,14 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { itemMock } from '../../fixture/itemFixture'
 import { MYSQL_CONNECTION } from '../../database/constants'
 import { Connection } from 'mysql2/promise'
-import { when } from 'jest-when'
-import { fileMock, filesMock } from '../../fixture/itemImageFixture'
+import { fileMock, filesMock, itemImageMock } from '../../fixture/itemImageFixture'
 import { ItemImageCreater } from './item-image.creater'
 import { ItemImageRepository } from '../../item-images/domain/item-image.repository'
 import { ItemRepository } from '../../items/domain/item.repository'
 
 describe('ItemImageCreater class', () => {
-  let itemImageService: ItemImageCreater
+  let itemImageCreater: ItemImageCreater
   let connection: Connection
 
   const ItemRepositoryMock = {
@@ -40,36 +39,29 @@ describe('ItemImageCreater class', () => {
       ],
     }).compile()
 
-    itemImageService = module.get<ItemImageCreater>(ItemImageCreater)
+    itemImageCreater = module.get<ItemImageCreater>(ItemImageCreater)
   })
 
-  describe('saveItemImages method', () => {
-    beforeEach(() => {
-      when(ItemRepositoryMock.save).calledWith(expect.anything()).mockResolvedValue(itemMock().id)
-      when(ItemRepositoryMock.findById).calledWith(itemMock().id).mockResolvedValue(itemMock())
-    })
+  describe('saveItemImage method', () => {
+    context('상품 id와 이미지 파일 정보, 대표이미지가 주어졌을때', () => {
+      it('메서드 호출을 검증해야 한다', async () => {
+        const spyFn = jest.spyOn(itemImageCreater, 'saveItemImage').mockImplementation()
+        await itemImageCreater.saveItemImage(itemMock().id, fileMock(), itemImageMock().isRepresentImage)
 
-    context('상품 정보와 이미지 파일이 주어지면', () => {
-      it('저장에 성공한다', async () => {
-        const id = await itemImageService.saveItemImages(itemMock().id, filesMock())
-
-        expect(id).toEqual(undefined)
+        expect(spyFn).toHaveBeenCalled()
+        expect(spyFn).toBeCalledWith(itemMock().id, fileMock(), itemImageMock().isRepresentImage)
       })
     })
   })
 
-  describe('saveItemImage method', () => {
-    beforeEach(() => {
-      when(ItemRepositoryMock.save).calledWith(expect.anything()).mockResolvedValue(itemMock().id)
-      when(ItemRepositoryMock.findById).calledWith(itemMock().id).mockResolvedValue(itemMock())
-    })
+  describe('saveItemImages method', () => {
+    context('상품 id와 여러개의 이미지 파일이 주어졌을때', () => {
+      it('메서드 호출을 검증해야 한다', async () => {
+        const spyFn = jest.spyOn(itemImageCreater, 'saveItemImages').mockImplementation()
+        await itemImageCreater.saveItemImages(itemMock().id, filesMock())
 
-    context('상품 정보와 이미지 파일이 주어지고 대표이미지가 true면', () => {
-      let isRepresentImage = true
-      it('저장에 성공한다', async () => {
-        const id = await itemImageService.saveItemImage(itemMock().id, fileMock(), isRepresentImage)
-
-        expect(id).toEqual(undefined)
+        expect(spyFn).toHaveBeenCalled()
+        expect(spyFn).toBeCalledWith(itemMock().id, filesMock())
       })
     })
   })
