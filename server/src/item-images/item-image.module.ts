fd432bf91e0626de4d0e6d7e3e4af62d7ae28e9e
jihwooon <jihwooon@gmail.com>
2023-11-02 11:16:19 +0900

    ItemImageModule를 공통화 하라
    ItemModule과 ItemImageModule에 공통으로 사용하는 Module를 ItemImageModule에 공통화 하고 다시 모듈 내보내기를 했습니다.

--- server/src/item-images/item-image.module.ts
+++ server/src/item-images/item-image.module.ts
@@ -4,10 +4,20 @@ import { ItemImageCreater } from '../item-images/application/item-image.creater'
 import { ItemImageRepository } from '../item-images/domain/item-image.repository'
 import { ItemImageUpdater } from '../item-images/application/item-image.updater'
 import { ItemRepository } from '../items/domain/item.repository'
+import { MemberModule } from '../members/member.module'
+import { JwtModule } from '../jwt/jwt.module'
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, MemberModule, JwtModule],
   providers: [ItemImageCreater, ItemImageRepository, ItemImageUpdater, ItemRepository],
-  exports: [ItemImageCreater, ItemImageRepository, ItemImageUpdater, ItemRepository],
+  exports: [
+    ItemImageCreater,
+    ItemImageRepository,
+    ItemImageUpdater,
+    ItemRepository,
+    DatabaseModule,
+    MemberModule,
+    JwtModule,
+  ],
 })
 export class ItemImageModule {}
--- server/src/items/item.module.ts
+++ server/src/items/item.module.ts
@@ -4,15 +4,12 @@ import { ItemReader } from './application/item.reader'
 import { ItemCreateController } from './web/item-create.controller'
 import { ItemDetailController } from './web/item-detail.controller'
 import { ItemRepository } from './domain/item.repository'
-import { DatabaseModule } from '../config/database/database.module'
 import { ItemUpdateController } from './web/item-update.controller'
 import { ItemUpdater } from './application/item.updater'
 import { ItemImageModule } from '../item-images/item-image.module'
-import { MemberModule } from '../members/member.module'
-import { JwtModule } from '../jwt/jwt.module'
 
 @Module({
-  imports: [DatabaseModule, ItemImageModule, MemberModule, JwtModule],
+  imports: [ItemImageModule],
   controllers: [ItemCreateController, ItemDetailController, ItemUpdateController],
   providers: [ItemCreater, ItemRepository, ItemReader, ItemUpdater],
 })
